c440a1c6f044afd8b212240d9e24961a5181e1c4
Arefun Naher Nipa <arefunnnipa@gmail.com>
2024-09-22 23:50:52 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+
+// let age = document.querySelector(".number")
+// let button = document.querySelector("button")
+
+// let txt = document.querySelector("h2")
+
+// button.addEventListener("click", function () {
+//     if (age.value >= 18) {
+//         // console.log("You Are Adult");
+//         txt.innerHTML = "You Are Adult"
+//         txt.style.color = "green"
+
+//     } 
+//     else {
+//         // console.log("You Are Baby");
+//         txt.innerHTML = "You Are Baby"
+//         txt.style.color = "red"
+//     }
+
+// })
+
+
+
+
+
+let gender = document.querySelector(".gender")
+let area = document.querySelector(".area")
+let age = document.querySelector(".age")
+let button = document.querySelector("button")
+let txt = document.querySelector("h2")
+
+button.addEventListener("click", function () {
+    if (gender.value === "female") {
+        if (area.value === "bogura") {
+            if (age.value >= 21 && age.value <= 30) {
+                // console.log("You Are Perfect Candidate");
+                txt.innerHTML = "You Are Perfect Candidate"
+                txt.style.color = "green"
+            }
+
+            else {
+                // console.log("Age not Match");
+                txt.innerHTML = "Age not Match"
+                txt.style.color = "red"
+            }
+        }
+        else {
+            // console.log("Must Entry bogura but Your entry wrong");
+                txt.innerHTML = "Must Entry bogura but Your entry wrong"
+                txt.style.color = "red"
+        }
+    }
+    else {
+        // console.log("Must Entry Female but Your entry wrong");
+                txt.innerHTML = "Must Entry Female but Your entry wrong"
+                txt.style.color = "red"
+    }
+
+
+})
+
